bf9563a870301f233838ebe2e069a57312b883de
avishay-gs <avishay@grip.security>
2022-10-28 12:50:15 +0300

    test refactor

--- conjugator.test.js
+++ conjugator.test.js
@@ -30,47 +30,52 @@ describe('Conjugator', function () {
     })
 
     describe("להישתלח", () => {
-        let pageData;
+        let pageData = {}
 
         beforeAll(async () => {
             await page.goto("https://www.pealim.com/dict/2222-lehishtaleach/");
-            pageData = await page.evaluate(scrape);
+            pageData = (await page.evaluate(scrape))
+                .reduce((data, row) => {
+                    data[row[IDX.tense]] = row;
+                    return data;
+                });
         });
 
         test('Should parse verb correctly - sanity', async () => {
-            const a = pageData.find((d) => d[IDX.tense] === "Imperfect 2nd ♀️ Plural")
+            const a = pageData["Imperfect 2nd ♀️ Plural"];
             expect(a[IDX.verb]).toEqual("<div><span class=\"menukad\">תִּשְׁתַּלַּחְנָה</span></div><div class=\"transcription\">tishtal<b>a</b>chna</div>");
         });
 
         describe('HebrewOnly', function () {
             test('Should handle multiple nikudim', async () => {
-                const a = pageData.find((d) => d[IDX.tense] === "Perfect 3rd ♂️ Sing.");
+                const a = pageData["Perfect 3rd ♂️ Sing."];
                 expect(a[IDX.onlyHebrew]).toEqual("הִשְׁתַּלֵּחַ, הִשְׁתַּלַּח");
             })
 
             test('Should handle special characters', async () => {
-                const a = pageData.find((d) => d[IDX.tense] === "Imperfect 2nd ♀️ Plural");
+                const a = pageData["Imperfect 2nd ♀️ Plural"];
                 expect(a[IDX.onlyHebrew]).toEqual("תִּשְׁתַּלַּחְנָה");
             })
         });
 
 
-        test('Should handle rlm and exclamation marks', async () => {
-            const a = pageData.find((d) => d[IDX.tense] === "Perfect 3rd ♂️ Sing.");
-            const b = pageData.find((d) => d[IDX.tense] === "Imperative 2nd ♂️ Sing.")
-            expect(a[IDX.verb]).toEqual(b[IDX.verb]);
-            expect(a[IDX.additionalTense]).toEqual(b[IDX.tense]);
-            expect(b[IDX.hideReverse]).toBeTruthy();
-        });
+        describe("Dedup", () => {
+            test('Naive Case', async () => {
+                const a = pageData["Imperfect 2nd ♀️ Plural"];
+                const b = pageData["Imperfect 3rd ♀️ Plural"];
+                expect(a[IDX.verb]).toEqual(b[IDX.verb]);
+                expect(a[IDX.additionalTense]).toEqual(b[IDX.tense]);
+                expect(b[IDX.hideReverse]).toBeTruthy();
+            });
 
-
-        test('Should find duplicates - naive case', async () => {
-            const a = pageData.find((d) => d[IDX.tense] === "Imperfect 2nd ♀️ Plural");
-            const b = pageData.find((d) => d[IDX.tense] === "Imperfect 3rd ♀️ Plural")
-            expect(a[IDX.verb]).toEqual(b[IDX.verb]);
-            expect(a[IDX.additionalTense]).toEqual(b[IDX.tense]);
-            expect(b[IDX.hideReverse]).toBeTruthy();
-        });
+            test('Should handle rlm and exclamation marks', async () => {
+                const a = pageData["Perfect 3rd ♂️ Sing."];
+                const b = pageData["Imperative 2nd ♂️ Sing."];
+                expect(a[IDX.verb]).toEqual(b[IDX.verb]);
+                expect(a[IDX.additionalTense]).toEqual(b[IDX.tense]);
+                expect(b[IDX.hideReverse]).toBeTruthy();
+            });
+        })
     });
 
     afterAll(() => browser.close());
